5e4246a1732a57ba4e2f0c89667ce5b65560b273
matias briso <briso.matias@gmail.com>
2025-03-14 00:30:04 -0300

    move state

--- src/app/personajes/state/personajesState.tsx
+++ src/app/personajes/state/personajesState.tsx
@@ -1,10 +1,10 @@
 "use client";
 import { useEffect, useState } from "react";
-import { personajesContext } from "./personajesContext";
+import { personajesContext } from "../context/personajesContext";
 import { getPersonajes } from "../service/Personaje.services";
 import { arrayChunk } from "@/app/shared/tools/generic";
 
-export function PersonajesState({children}) {
+export function PersonajesState({ children }) {
     const [personajes, setPersonajes] = useState([]);
     const setPersonajeState = async (page: number) => {
         let response = await getPersonajes(page);
