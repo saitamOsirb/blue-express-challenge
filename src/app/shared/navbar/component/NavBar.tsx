5a09f0ae3e57f555c0a92197ee17cc9a2067fabf
matias briso <briso.matias@gmail.com>
2025-03-17 08:10:22 -0300

    fix navbar

--- src/app/shared/navbar/component/NavBar.tsx
+++ src/app/shared/navbar/component/NavBar.tsx
@@ -1,8 +1,32 @@
 "use client"
 
-export function NavBar() {
+import { useEffect, useState } from "react";
 
-  return <div className="navbar bg-primary text-primary-content">
+export function NavBar() {
+  const [width, setWidth] = useState(0);
+  const updateDimensions = () => {
+    setWidth(window.innerWidth);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", updateDimensions);
+    return () => window.removeEventListener("resize", updateDimensions);
+  }, null);
+  return <div>
+{
+    width < 450 &&
+    <div className="navbar bg-primary text-primary-content">
+    <div className="flex-1">
+    <ul className="menu menu-horizontal px-1">
+          <li><a href="/personajes">Personajes</a></li>
+          <li><a href="/ubicaciones">Ubicaciones</a></li>
+          <li><a href="/episodios">Episodios</a></li>
+        </ul>
+    </div>
+  </div>
+  }
+  {
+    width > 450 &&
+    <div className="navbar bg-primary text-primary-content">
     <div className="flex-1">
       <a className="btn btn-ghost text-xl">Bluexpress Challenge</a>
     </div>
@@ -16,4 +40,25 @@ export function NavBar() {
       </div>
     </div>
   </div>
+  }
+  </div>
+  
+  /*
+*/
+
+
+  /*<div className="navbar bg-primary text-primary-content">
+    <div className="flex-1">
+      <a className="btn btn-ghost text-xl">Bluexpress Challenge</a>
+    </div>
+    <div className="flex-none">
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li><a href="/personajes">Personajes</a></li>
+          <li><a href="/ubicaciones">Ubicaciones</a></li>
+          <li><a href="/episodios">Episodios</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>*/
 }
