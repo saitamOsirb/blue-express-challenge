90bf2928a5f8c7863f47a478ef4c7c76c6abfe4c
matias briso <briso.matias@gmail.com>
2025-03-17 08:12:37 -0300

    fix navbar

--- src/app/shared/navbar/component/NavBar.tsx
+++ src/app/shared/navbar/component/NavBar.tsx
@@ -42,23 +42,4 @@ export function NavBar() {
   </div>
   }
   </div>
-  
-  /*
-*/
-
-
-  /*<div className="navbar bg-primary text-primary-content">
-    <div className="flex-1">
-      <a className="btn btn-ghost text-xl">Bluexpress Challenge</a>
-    </div>
-    <div className="flex-none">
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><a href="/personajes">Personajes</a></li>
-          <li><a href="/ubicaciones">Ubicaciones</a></li>
-          <li><a href="/episodios">Episodios</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>*/
 }
