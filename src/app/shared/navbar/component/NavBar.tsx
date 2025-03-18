810ea0e479d1138aa485701572dc99bef0917749
matias briso <briso.matias@gmail.com>
2025-03-17 21:10:08 -0300

    fix size window responsive

--- src/app/shared/navbar/component/NavBar.tsx
+++ src/app/shared/navbar/component/NavBar.tsx
@@ -1,45 +1,35 @@
 "use client"
-
-import { useEffect, useState } from "react";
+import useWindowSize from "@rooks/use-window-size";
 
 export function NavBar() {
-  const [width, setWidth] = useState(0);
-  const updateDimensions = () => {
-    setWidth(window.innerWidth);
-  }
-  useEffect(() => {
-    window.addEventListener("resize", updateDimensions);
-    return () => window.removeEventListener("resize", updateDimensions);
-  }, null);
-  return <div>
-{
-    width < 450 &&
-    <div className="navbar bg-primary text-primary-content">
-    <div className="flex-1">
-    <ul className="menu menu-horizontal px-1">
-          <li><a href="/personajes">Personajes</a></li>
-          <li><a href="/ubicaciones">Ubicaciones</a></li>
-          <li><a href="/episodios">Episodios</a></li>
-        </ul>
-    </div>
-  </div>
-  }
-  {
-    width > 450 &&
-    <div className="navbar bg-primary text-primary-content">
-    <div className="flex-1">
-      <a className="btn btn-ghost text-xl">Bluexpress Challenge</a>
-    </div>
-    <div className="flex-none">
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li><a href="/personajes">Personajes</a></li>
-          <li><a href="/ubicaciones">Ubicaciones</a></li>
-          <li><a href="/episodios">Episodios</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  }
-  </div>
+  const { innerWidth } = useWindowSize();
+  return (
+    <header>
+      {innerWidth <= 450
+        ? <div className="navbar bg-primary text-primary-content">
+          <div className="flex-1">
+            <ul className="menu menu-horizontal px-1">
+              <li><a href="/personajes">Personajes</a></li>
+              <li><a href="/ubicaciones">Ubicaciones</a></li>
+              <li><a href="/episodios">Episodios</a></li>
+            </ul>
+          </div>
+        </div>
+        : <div className="navbar bg-primary text-primary-content">
+          <div className="flex-1">
+            <a className="btn btn-ghost text-xl">Bluexpress Challenge</a>
+          </div>
+          <div className="flex-none">
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1">
+                <li><a href="/personajes">Personajes</a></li>
+                <li><a href="/ubicaciones">Ubicaciones</a></li>
+                <li><a href="/episodios">Episodios</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      }
+    </header>
+  );
 }
--- src/app/shared/tools/generic.ts
+++ src/app/shared/tools/generic.ts
@@ -1,15 +1,7 @@
 
-
-
 export const arrayChunk = (arr: unknown[], n: number) => {
     const array = arr.slice();
     const chunks = [];
     while (array.length) chunks.push(array.splice(0, n));
     return chunks;
 };
-/*
-function paginate(array: unknown[], page_size: number, page_number: number) {
-    // human-readable page numbers usually start with 1, so we reduce 1 in the first argument
-    return array.slice((page_number - 1) * page_size, page_number * page_size);
-}
-*/
